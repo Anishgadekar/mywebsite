08d4d8cfcf72f75c6388e5236c866b6f965a4c83
Anish Gadekar <anishgoa1@gmail.com>
2019-01-07 21:56:58 +0530

    updated cv

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@ $(function () {
   
   var prog = $(".progress-bar");
   
-prog.eq(0).animate({width:"80%"}, 2000)
+prog.eq(0).animate({width:"85%"}, 2000)
 
 prog.eq(1).animate({width:"75%"}, 2000)
 
@@ -22,9 +22,9 @@ prog.eq(6).animate({width:"60%"}, 2000)
 
 prog.eq(7).animate({width:"50%"}, 2000)
 
-prog.eq(8).animate({width:"50%"}, 2000)
+prog.eq(8).animate({width:"60%"}, 2000)
 
-prog.eq(9).animate({width:"50%"}, 2000)
+prog.eq(9).animate({width:"80%"}, 2000)
 
 prog.eq(10).animate({width:"50%"}, 2000)
 
